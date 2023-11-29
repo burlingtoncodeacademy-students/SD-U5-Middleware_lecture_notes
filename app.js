0ab7342643b9dd4403fdb3f1acf35226772f7252
Eric Winebrenner <ejwinebrenner@gmail.com>
2023-11-29 16:10:13 -0500

    updated file structure

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 4000;
-const practiceController = require('./controllers/practice.controller');
 
 //* Middleware
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 //* Controllers
-app.use('/practice', practiceController);
 
 //* Port 
 app.listen(PORT, () => {
